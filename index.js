0cc6916162dad0b31842b0be2231b557159bfbad
ameermavia <maviaking97@gmail.com>
2024-02-22 02:58:07 +0500

    done

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 const express = require('express')
 require('dotenv').config()
-
-
+const cors = require('cors')
 const app = express()
 
+
+
+app.use(express.json());
+app.use(cors())
+
 const port = process.env.PORT
-const LOGINUSERNAME =  process.env.LOGINNAMES
-const USERNAME = process.env.NAMES
+
+
+const Todo = []
+
+
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello World!<h1/>`)
+   res.send(`<h1>Hello World!<h1/>`)
 })
 
-app.get('/home' , (req , res)=>{
-    res.send(`<h1>Hello ${USERNAME}<h1/>`)
+app.get('/app/v1/todo' , (req , res)=>{
+  res.send(Todo)
 })
 
-app.get('/login' , (req , res)=>{
-    res.send(`<h1> Hello ${LOGINUSERNAME}<h1/>`)
+app.post('/app/v1/todo' , (req , res)=>{
+    const title = req.body.title
+    Todo.push(
+      {
+        title: title,
+        id: Date.now()
+      }
+    )
+    res.send('TITLE ADD SUCCESSFULLY')
+
 })
+
+app.delete('/app/v1/todo:id' , (req , res)=>{
+   const id = req.params.id
+   const index = Todo.findIndex((Todo)=> Todo.id === Number(id) )
+   if(index === -1){
+    res.send('user not found')
+    return
+   }
+   Todo.splice(index , 1)
+   res.send('delete')
+})
+
+app.put('/app/v1/todo:id' , (req , res)=>{
+  const title = req.body.title
+  const id = req.params.id
+   const index = Todo.findIndex((Todo)=> Todo.id === Number(id) )
+   if(index === -1){
+    res.send('user not found')
+    return
+   }
+   Todo[index].title = title
+   res.send('Edit succesfullt')
+  
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
